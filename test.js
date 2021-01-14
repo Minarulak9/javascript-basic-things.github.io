8b9de28d0b233ed62b1fb18c9be65ec60a523c68
MinarulAk <minarulak88@gmail.com>
2021-01-14 20:38:46 +0530

    add gitignore

--- test.js
+++ test.js
@@ -1,24 +1,27 @@
 let user = [
-    {
-        name:"minarulak",
-        pass:"mak4256"
-    },
-    {
-        name:"rocky",
-        pass:"mak56"
+  {
+    name: "minarulak",
+    pass: "mak4256",
+  },
+  {
+    name: "rocky",
+    pass: "mak56",
+  },
+];
+let authUser = (username, pass) => {
+  for (i = 0; i < user.length; i++) {
+    if (user[i].name == username && user[i].pass == pass) {
+      console.log(username + "loged in......");
+      return;
     }
-]
-let authUser=(username,pass)=>{
-   for(i=0;i<user.length;i++){
-        if (user[i].name == username && user[i].pass == pass) {
-            console.log(username + "loged in......");
-            return;
-        }
-    }
-    console.log("incorect username and password");
-}
-let user =[{
-    name:"minarulak",
-    pass:"mak4256"
-}]
-localStorage.setItem("data",JSON.stringify(user))
+  }
+  console.log("incorect username and password");
+};
+let arr = [2,100,100,200,500,25,652,6546,54,54,2,145,356]
+let mak= []
+arr.forEach(ele=>{
+  if (!mak.includes(ele)) {
+    mak.push(ele)
+  }
+})
+console.log(mak);
